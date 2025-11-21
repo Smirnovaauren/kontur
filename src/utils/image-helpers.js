bb3b93690c8150e10e210ff792ba78f299b688ca
ouzich <ouzich@ac-sw.com>
2025-11-21 09:47:12 +0700

    добавила изображения в папку dist

--- src/utils/image-helpers.js
+++ src/utils/image-helpers.js
@@ -8,3 +8,4 @@ export function buildImagePath(fileName, type = IMAGE_TYPES.gallery) {
   // Используем new URL, чтобы Vite пометил картинку как зависимость и скопировал её в dist при сборке
   return new URL(`../assets/images/${type}/${fileName}`, import.meta.url).href
 }
+gi
